fc7722434b699dcc4672edd4731a6aba4b17bef6
Thorsten Hans <thorsten.hans@gmail.com>
2017-09-17 14:32:39 +0200

    Make it a bit configurable using env vars

---
+++ code/Config/InMemory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace Demo.IdentityServer.Config
+{
+    public class InMemory
+    {
+        
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = System.Environment.GetEnvironmentVariable("IdSrvSubject"),
+                    Username = System.Environment.GetEnvironmentVariable("IdSrvUserName"),
+                    Password = System.Environment.GetEnvironmentVariable("IdSrvPassword")
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            yield return new ApiResource(System.Environment.GetEnvironmentVariable("IdSrvApiScopeName"), "API");
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            yield return new Client
+            {
+                AccessTokenType = AccessTokenType.Jwt,
+                Enabled = true,
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                ClientId = System.Environment.GetEnvironmentVariable("IdSrvClientId"),
+                ClientSecrets =
+                {
+                    new Secret(System.Environment.GetEnvironmentVariable("IdSrvClientSecret").Sha256())
+                },
+                AllowedScopes = {System.Environment.GetEnvironmentVariable("IdSrvApiScopeName")}
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            yield return new IdentityResources.OpenId();
+            yield return new IdentityResources.Profile();
+        }
+    }
+}
